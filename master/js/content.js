6616c83220fd7981ce79ed31970a1018c96c352b
MadelineWu <wumadeline@berkeley.edu>
2015-04-10 15:48:56 -0700

    For calnet login

--- master/js/content.js
+++ master/js/content.js
@@ -1,2 +1,16 @@
 // Javascript File
 
+var username;
+var password;
+
+// creating an alert on the page
+//alert("Your password is 123456");
+
+$(document).ready(function(){
+	// code
+	if ($('.error').length == 0) {
+		$('input#username').val('your username')
+		$('input#password').val('your password')
+		$('input.button').click();
+	}
+});
